352cf3edcb06bf4cb34ea12f62e52a3fb7268f35
Iliya Lunev <lunevig90@gmail.com>
2021-09-20 13:15:02 +0300

    проверка Реакта

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// c подключеникм импорта Реакт
+// const el = React.createElement('div', { a: 5, b: 6, children: 'Привет Реакт' });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const jsxEl1 = <span>Привет</span>;
+const jsxEl2 = <p>Мир</p>;
+
+const jsxEl = (
+  <div>
+    {jsxEl1}
+    {jsxEl2}
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(jsxEl, document.querySelector('#root'));
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
