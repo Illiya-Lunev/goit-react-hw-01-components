94ecfc9ddad1af700e495ff9f8f124b639bfddae
Iliya Lunev <lunevig90@gmail.com>
2021-09-21 15:47:00 +0300

    подготовлен реакт к дз

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
-import ReactDOM from 'react-dom';
-// c подключеникм импорта Реакт
-// const el = React.createElement('div', { a: 5, b: 6, children: 'Привет Реакт' });
-
-const jsxEl1 = <span>Привет</span>;
-const jsxEl2 = <p>Мир</p>;
-
-const jsxEl = (
-  <div>
-    {jsxEl1}
-    {jsxEl2}
-  </div>
-);
-
-ReactDOM.render(jsxEl, document.querySelector('#root'));
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+// import ReactDOM from 'react-dom';
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -23,8 +6,3 @@ ReactDOM.render(jsxEl, document.querySelector('#root'));
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
