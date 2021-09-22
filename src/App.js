f14520916408cd9e7d223d70862f1c9c7fa8ffce
Iliya Lunev <lunevig90@gmail.com>
2021-09-22 15:02:34 +0300

    add Transactions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import statisticalData from './componets/Statisctick/statistical-data.json';
 // Компонент друзей
 import Friends from './componets/Friends/Friends';
 import friendsData from './componets/Friends/Friends-data.json';
+// Компонент Транзакций
+import transactions from './componets/Transaction/transactions.json';
+import TransactionHistory from './componets/Transaction/TransactionHistory';
 
 export default function App() {
   return (
@@ -20,9 +23,9 @@ export default function App() {
         likes={user.stats.likes}
         views={user.stats.views}
       />
-
       <Statistics title="Upload stats" stats={statisticalData} />
       <Friends friends={friendsData} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
